b27f9c0fff7f9e5735faa22d36382a7314088aeb
penice909 <138809757+penice909@users.noreply.github.com>
2026-02-02 21:40:47 +0600

    Delete src/userplugins directory

--- src/userplugins/randomGary/native.ts
+++
@@ -1,7 +0,0 @@
-export async function getImageBuffer(_, url: string) {
-    const response = await fetch(url);
-    const arrayBuffer = await response.arrayBuffer();
-    return Buffer.from(arrayBuffer);
-
-}
-
